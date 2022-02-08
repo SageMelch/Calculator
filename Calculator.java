b2a150d8763ebc37565ad5a5caec17341c28703c
Sage <98679802+SageMelch@users.noreply.github.com>
2022-02-07 17:51:26 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,44 +8,59 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter First Digit: ");
         double num1 = scanner.nextDouble();
-        System.out.print("Enter Second Digit: ");
-        double num2 = scanner.nextDouble();
-        //Variables
-        long power = (long) num2 - 1; //For Exponentiation Conversion
-        int exponent = (int)power;//Argument in for loop
-        double num4 = num1;     //Keeps num1 the same
+
         //Operators
         System.out.println("Enter Operation: ");
-        System.out.println("1 - Add");
-        System.out.println("2 - Subtract");
-        System.out.println("3 - Multiply");
-        System.out.println("4 - Divide");
-        System.out.println("5 - Exponentiate (Second digit will round down to the nearest integer)");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraction");
+        System.out.println("3 - Multiplication");
+        System.out.println("4 - Division");
+        System.out.println("5 - Exponentiation (Second digit will round down to nearest integer)");
+        System.out.println("6 - Factorial");
         int operator = scanner.nextInt();
-        //Calculations
-        if(operator == 1) {
-            System.out.println("Result: "+ (num1 + num2));
-        }else if(operator == 2){
-            System.out.println("Result: "+ (num1 - num2));
-        }else if(operator == 3){
-            System.out.println("Result: "+ (num1 * num2));
-        }else if(operator == 4) {
-            if (num2 == 0) {
-                System.out.println("Result: Undefined");
-            } else
-                System.out.println("Result: " + (num1 / num2));
-        }else if(operator == 5){
-            if(num2 == 0){
+        //Doesn't need second input
+        if(operator == 6){
+            if(num1 == 0){
                 System.out.println("Result: 1");
             }else{
-                for(int i=0;i<exponent;i++){
-                    num4 *= num1;
+                int factorial = (int) num1 - 1;
+                for(int i=factorial;i>0;i--){
+                    num1 *= i;
                 }
-                System.out.println("Result: "+num4);
+                System.out.println("Result: "+num1);
             }
-
         }else{
-            System.out.println("Not a valid operator");
+            System.out.print("Enter Second Digit: ");
+            double num2 = scanner.nextDouble();
+            //Variables
+            long power = (long) num2 - 1; //For Exponentiation Conversion
+            int exponent = (int)power;//Argument in for loop
+            double num4 = num1;     //Keeps num1 the same
+            //Calculations
+            if(operator == 1) {
+                System.out.println("Result: "+ (num1 + num2));
+            }else if(operator == 2){
+                System.out.println("Result: "+ (num1 - num2));
+            }else if(operator == 3){
+                System.out.println("Result: "+ (num1 * num2));
+            }else if(operator == 4) {
+                if (num2 == 0) {
+                    System.out.println("Result: Undefined");
+                } else
+                    System.out.println("Result: " + (num1 / num2));
+            }else if(operator == 5) {
+                if (num2 == 0) {
+                    System.out.println("Result: 1");
+                } else {
+                    for (int i = 0; i < exponent; i++) {
+                        num4 *= num1;
+                    }
+                    System.out.println("Result: " + num4);
+                }
+            }
+            else{
+                System.out.println("Not a valid operator");
+            }
         }
     }
 }
